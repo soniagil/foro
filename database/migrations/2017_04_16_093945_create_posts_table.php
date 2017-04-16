b7d9e417a32cb078d6fead7e2d5d39a20e5c0e5b
Sonia Gil <sgilbenitez@gmail.com>
2017-04-16 19:29:31 +0200

    Add unit integration and functional tests to prove that creating a post generates a slug

--- database/migrations/2017_04_16_093945_create_posts_table.php
+++ database/migrations/2017_04_16_093945_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->string('title');
+            $table->string('slug');
             $table->mediumText('content');
             $table->boolean('pending')->default(true);
             $table->timestamps();
---
+++ tests/integration/PostIntegrationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Post;
+
+class PostIntegrationTest extends TestCase
+{
+	use DatabaseTransactions;
+
+    public function test_a_slug_is_generated_and_saved_to_the_database()
+    {
+    	$user = $this->defaultUser();
+
+        $post = factory(Post::class)->make([
+    		'title' => 'Como instalar Laravel'
+    	]);
+
+    	$user->posts()->save($post);
+
+    	/*$this->seeInDatabase('posts', [
+    		'slug' => 'como-instalar-laravel'
+    	]);
+
+    	$this->assertSame('como-instalar-laravel', $post->fresh()->slug);*/
+    	$this->assertSame('como-instalar-laravel', $post->slug);
+    }
+}
