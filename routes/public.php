2fea32fa81e33c0e85bdb84a88d277ce4544d101
Sonia Gil <sgilbenitez@gmail.com>
2017-04-16 20:29:11 +0200

    Redirect URLs with old slug to page with new slug

--- routes/public.php
+++ routes/public.php
@@ -8,7 +8,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('posts/{post}', [
+Route::get('posts/{post}-{slug}', [
 	'as' => 'posts.show',
 	'uses' => 'PostController@show'
 ])->where('post', '\d+'); // indicamos que el id del post sólo es numérico
--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -22,9 +22,30 @@ class ShowPostTest extends FeatureTestCase
     	$user->posts()->save($post);
 
     	// Cuando el usuario visite la página del post...
-    	$this->visit(route('posts.show', $post))
+    	$this->visit($post->url)
     		->seeInElement('h1', $post->title)
     		->see($post->content)
     		->see($user->name);
+    }
+
+    // urls obsoletas redirigen a las nuevas (por ejemplo,
+    // si se cambia el título de un post)
+    public function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->seePageIs($post->url);
     }
 }
